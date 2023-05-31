323111c958191cf41d99672ef9d993167b9e6c08
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-05-31 12:33:02 -0400

    cleanup game viewmodel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -2,7 +2,6 @@ package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
 import android.text.format.DateUtils
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
@@ -15,18 +14,32 @@ private val NO_BUZZ_PATTERN = longArrayOf(0)
 
 class GameViewModel : ViewModel() {
 
-    companion object {
-        private const val DONE = 0L
-        private const val ONE_SECOND = 1_000L
-        private const val COUNTDOWN_TIME = 20_000L
-    }
-
     enum class BuzzType(val pattern: LongArray) {
         CORRECT(CORRECT_BUZZ_PATTERN),
         GAME_OVER(GAME_OVER_BUZZ_PATTERN),
         COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
         NO_BUZZ(NO_BUZZ_PATTERN)
     }
+
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1_000L
+        private const val COUNTDOWN_TIME = 60_000L
+        private const val COUNTDOWN_PANIC_SECONDS = 10L
+    }
+
+    private val timer: CountDownTimer
+    private val _currentTime = MutableLiveData<Long>()
+
+    private lateinit var wordList: MutableList<String>
+
+    private val _eventBuzz = MutableLiveData<BuzzType>()
+    val eventBuzz: LiveData<BuzzType>
+        get() = _eventBuzz
+
+    private val _isGameOver = MutableLiveData<Boolean>()
+    val isGameOver: LiveData<Boolean>
+        get() = _isGameOver
 
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
@@ -36,35 +49,21 @@ class GameViewModel : ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
-    private val _isGameOver = MutableLiveData<Boolean>()
-    val isGameOver: LiveData<Boolean>
-        get() = _isGameOver
-
-    private val _currentTime = MutableLiveData<Long>()
-
     val currentTimeString = Transformations.map(_currentTime) { time ->
         DateUtils.formatElapsedTime(time)
     }
 
-    private val _eventBuzz = MutableLiveData<BuzzType>()
-    val eventBuzz: LiveData<BuzzType>
-        get() = _eventBuzz
-
-    private lateinit var wordList: MutableList<String>
-
-    private val timer: CountDownTimer
-
     init {
-        Log.i("GameViewModel", "GameViewModel created")
         resetList()
         nextWord()
         _score.value = 0
-        _currentTime.value = Long.MAX_VALUE
 
         timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
             override fun onTick(millisUntilFinished: Long) {
                 _currentTime.value = millisUntilFinished / ONE_SECOND
-                if ((millisUntilFinished / ONE_SECOND) < 10L) _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+                if ((millisUntilFinished / ONE_SECOND) < COUNTDOWN_PANIC_SECONDS) {
+                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+                }
             }
 
             override fun onFinish() {
@@ -133,6 +132,5 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         timer.cancel()
-        Log.i("GameViewModel", "onCleared: GameViewModel destroyed")
     }
 }
